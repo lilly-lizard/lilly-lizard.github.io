2b39b78bd830f5ef90a4593e9ffb6985a040ad39
lilly-lizard <bigbanana29@aol.com>
2025-07-04 22:02:58 +1200

    asd

--- notes.md
+++ notes.md
@@ -3,3 +3,40 @@
 [gif search](https://gifcities.org/)
 
 [2d javascript framework](https://pixijs.com)
+
+# plan
+
+electrons moving around circuit
+- just DC for now
+- electron speed * density = current aka electrons per second
+- electron color = voltage aka energy
+- power supply
+	- option to enable potential difference arrows
+	- show energy being added to circuit by v and i arrows aligning
+	- checkbox to reverse i arrows to show standard current direction
+- wires
+- resistor
+- somehow visually show low/high resistance struggle for electrons to move through
+- set amount of electrons per circuit
+	- no breaks in single electron continuity
+	- electrons packed back to back in series circuit
+	- electrons split down different paths at junction
+- want kids to think of electrons as marbles pushing on each other so space between electrons should be pretty much uniform throughout the circuit (just like irl I think)
+	- alternate idea...
+	- low r: wide, spread out and fast?
+	- high r: thin, bunched together and slow?
+	- na. seeing different speeds in series is confusing because it implies different currents.
+
+- set amount of electron entities
+	- total number can be adjusted via "global density" param
+	- screen space pos
+	- relative pos within current component
+	- energy/color
+- component
+	- type
+	- resistance
+	- density (spread out or bunched together)
+
+1. start out with rectangle and make electrons move around perimeter
+	- draw a rectangle
+	- draw wires
--- pages/circuit/circuit.js
+++ pages/circuit/circuit.js
@@ -1,33 +1,68 @@
-import { Application, Assets, Container, Sprite } from '/lib/pixi_v8.10.2.min.mjs'
+import { Application, Assets, Container, Graphics, Sprite } from '/lib/pixi_v8.10.2.min.mjs'
 
 (async () => {
-	const app = new Application();
-	await app.init({ background: '#0e1339', resizeTo: window });
 
-	// chuck the new canvas in the html document
-	document.body.appendChild(app.canvas);
+const app = new Application();
+const background_color = 0x0e1339;
+await app.init({ background: background_color, resizeTo: window });
 
-	// collection of bunnies
-	const bunnies = new Container();
-	app.stage.addChild(bunnies);
+// chuck the new canvas in the html document
+document.body.appendChild(app.canvas);
 
-	const texture = await Assets.load('/assets/bunny.png');
-	for (let i = 0; i < 25; i++) {
-		const bunny = new Sprite(texture);
-		bunny.x = (i % 5) * 40;
-		bunny.y = Math.floor(i / 5) * 40;
-		bunnies.addChild(bunny);
-	}
+const center_x = app.screen.width / 2;
+const center_y = app.screen.height / 2;
 
-	// place in center of screen
-	bunnies.x = app.screen.width / 2;
-	bunnies.y = app.screen.height / 2;
+draw_wire_rects();
 
-	// translate to center
-	bunnies.pivot.x = bunnies.width / 2;
-	bunnies.pivot.y = bunnies.height / 2;
+function draw_wire_rects() {
+	// draw rectangle https://pixijs.com/8.x/guides/migrations/v8#graphics-api-overhaul
+	const wire_thickness = 30;
+	const circuit_height = 300;
+	const circuit_width = 600;
+	const border_color = 0x505050;
+	const border_thickness = 2;
 
-	app.ticker.add((time) => {
-		bunnies.rotation -= 0.01 * time.deltaTime;
-	});
+	var rect_1 = new Graphics();
+	rect_1.rect(center_x - circuit_width / 2, center_y - circuit_height / 2, circuit_width, circuit_height);
+	rect_1.fill(0x000000);
+	rect_1.stroke({ width: border_thickness, color: border_color });
+	app.stage.addChild(rect_1);
+
+	var rect_2 = new Graphics();
+	rect_2.rect(center_x - circuit_width / 2 + wire_thickness, center_y - circuit_height / 2 + wire_thickness,
+		circuit_width - wire_thickness * 2, circuit_height - wire_thickness * 2);
+	rect_2.fill(background_color);
+	rect_2.stroke({ width: border_thickness, color: border_color });
+	app.stage.addChild(rect_2);
+}
+
 })();
+
+// ARCHIVE
+
+function draw_separate_wire_rectangles() {
+	// draw rectangle https://pixijs.com/8.x/guides/migrations/v8#graphics-api-overhaul
+	// var wire_thickness = 40;
+	// var circuit_height = 300;
+	// var circuit_width = 600;
+	
+	// var rect_1 = new Graphics(); // top
+	// rect_1.rect(center_x - circuit_width / 2, center_y - circuit_height / 2, circuit_width, wire_thickness);
+	// rect_1.fill(0x000000);
+	// app.stage.addChild(rect_1);
+
+	// var rect_2 = new Graphics(); // bottom
+	// rect_2.rect(center_x - circuit_width / 2, center_y + circuit_height / 2, circuit_width + wire_thickness, wire_thickness);
+	// rect_2.fill(0x000000);
+	// app.stage.addChild(rect_2);
+
+	// var rect_3 = new Graphics(); // left
+	// rect_3.rect(center_x - circuit_width / 2, center_y - circuit_height / 2, wire_thickness, circuit_height);
+	// rect_3.fill(0x000000);
+	// app.stage.addChild(rect_3);
+
+	// var rect_4 = new Graphics(); // right
+	// rect_4.rect(center_x + circuit_width / 2, center_y - circuit_height / 2, wire_thickness, circuit_height + wire_thickness);
+	// rect_4.fill(0x000000);
+	// app.stage.addChild(rect_4);
+}
